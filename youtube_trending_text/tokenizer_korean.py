f0725f32b663b1eb60d279a63c240538a5f8676e
minwook-shin <minwook0106@gmail.com>
2024-03-09 19:27:47 +0900

    add tokenizer_korean feature

---
+++ youtube_trending_text/tokenizer_korean.py
@@ -0,0 +1,53 @@
+import time
+
+import transformers
+from konlpy.tag import Mecab, Komoran
+from transformers import T5Tokenizer
+import logging
+
+transformers.logging.set_verbosity_error()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(console_handler)
+
+
+class TokenizerComparison:
+    def __init__(self, text):
+        self.text = text
+        self.hf_sp_bpe = T5Tokenizer.from_pretrained('digit82/kolang-t5-base')
+        self.mecab = Mecab()
+        self.ko_moran = Komoran()
+
+    def sentencepiece(self):
+        start = time.time()
+        tokens = self.hf_sp_bpe.tokenize(self.text)
+        end = time.time()
+        logger.debug(f"SentencePiece Tokenization Time: {end - start} seconds : {tokens}")
+        return tokens
+
+    def konlpy_mecab(self):
+        start = time.time()
+        tokens = self.mecab.morphs(self.text)
+        end = time.time()
+        logger.debug(f"KoNLPy Mecab Tokenization Time: {end - start} seconds : {tokens}")
+        return tokens
+
+    def konlpy_komoran(self):
+        start = time.time()
+        tokens = self.ko_moran.morphs(self.text)
+        end = time.time()
+        logger.debug(f"KoNLPy KOMORAN Tokenization Time: {end - start} seconds : {tokens}")
+        return tokens
+
+
+if __name__ == "__main__":
+    sample_text = "아버지가방에들어가신다."
+    tokenizer_comparison = TokenizerComparison(sample_text)
+    print(tokenizer_comparison.sentencepiece())
+    print(tokenizer_comparison.konlpy_mecab())
+    print(tokenizer_comparison.konlpy_komoran())
