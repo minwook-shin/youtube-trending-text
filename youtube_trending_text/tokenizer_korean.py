54362ae9c22fd3321e6a749ab74a9f526fd16f93
minwook-shin <minwook0106@gmail.com>
2024-03-09 22:51:15 +0900

    add timer trigger

--- youtube_trending_text/tokenizer_korean.py
+++ youtube_trending_text/tokenizer_korean.py
@@ -23,31 +23,43 @@ class TokenizerComparison:
         self.mecab = Mecab()
         self.ko_moran = Komoran()
 
-    def sentencepiece(self):
-        start = time.time()
-        tokens = self.hf_sp_bpe.tokenize(self.text)
-        end = time.time()
-        logger.debug(f"SentencePiece Tokenization Time: {end - start} seconds : {tokens}")
+    def sentencepiece(self, timer=False):
+        if timer:
+            start = time.time()
+            tokens = self.hf_sp_bpe.tokenize(self.text)
+            end = time.time()
+            logger.debug(f"SentencePiece Tokenization Time: {end - start} seconds : {tokens}")
+        else:
+            tokens = self.hf_sp_bpe.tokenize(self.text)
+            logger.debug(f"SentencePiece Tokenization : {tokens}")
         return tokens
 
-    def konlpy_mecab(self):
-        start = time.time()
-        tokens = self.mecab.morphs(self.text)
-        end = time.time()
-        logger.debug(f"KoNLPy Mecab Tokenization Time: {end - start} seconds : {tokens}")
+    def konlpy_mecab(self, timer=False):
+        if timer:
+            start = time.time()
+            tokens = self.mecab.morphs(self.text)
+            end = time.time()
+            logger.debug(f"KoNLPy Mecab Tokenization Time: {end - start} seconds : {tokens}")
+        else:
+            tokens = self.mecab.morphs(self.text)
+            logger.debug(f"KoNLPy Mecab Tokenization : {tokens}")
         return tokens
 
-    def konlpy_komoran(self):
-        start = time.time()
-        tokens = self.ko_moran.morphs(self.text)
-        end = time.time()
-        logger.debug(f"KoNLPy KOMORAN Tokenization Time: {end - start} seconds : {tokens}")
+    def konlpy_komoran(self, timer=False):
+        if timer:
+            start = time.time()
+            tokens = self.ko_moran.morphs(self.text)
+            end = time.time()
+            logger.debug(f"KoNLPy KOMORAN Tokenization Time: {end - start} seconds : {tokens}")
+        else:
+            tokens = self.ko_moran.morphs(self.text)
+            logger.debug(f"KoNLPy KOMORAN Tokenization : {tokens}")
         return tokens
 
 
 if __name__ == "__main__":
     sample_text = "아버지가방에들어가신다."
     tokenizer_comparison = TokenizerComparison(sample_text)
-    print(tokenizer_comparison.sentencepiece())
-    print(tokenizer_comparison.konlpy_mecab())
-    print(tokenizer_comparison.konlpy_komoran())
+    tokenizer_comparison.sentencepiece(timer=True)
+    tokenizer_comparison.konlpy_mecab(timer=True)
+    tokenizer_comparison.konlpy_komoran(timer=True)
